d91405c84d32d65efe69acbf3a803d1caf13f2e6
Jidaewook <psatdoctor@naver.com>
2020-05-24 12:08:09 +0900

    Client Page Basic Link

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Nav from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './auth/Login';
 
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-     
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path="/" component={Landing} />
+        <div class="container">
+          <Route exact path="/login" component={Login} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Landing = () => {
     return (
@@ -14,9 +15,9 @@ const Landing = () => {
                                 </p>
                                 <hr />
                                 
-                                <a href="login.html" className="btn btn-lg btn-light">
+                                <Link to="/login" className="btn btn-lg btn-light">
                                     Login
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
