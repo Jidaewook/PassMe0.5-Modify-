0129644f968d4c858e4032bab6b4ad0ba0323abc
Jidaewook <psatdoctor@naver.com>
2020-05-31 14:25:59 +0900

    Add Redux store & Chrome extention

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import Footer from './components/layout/Footer';
 import Nav from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './auth/Login';
+import Dashboard from './components/Dashboard';
+import Store from './store';
 
 import './App.css';
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Route exact path="/" component={Landing} />
-        <div class="container">
-          <Route exact path="/login" component={Login} />
+    <Provider store={Store}>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={Landing} />
+          <div class="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
