33473e55bf8c1d2148480aaed2f48bd71b543a88
Jidaewook <psatdoctor@naver.com>
2020-06-06 20:14:54 +0900

    modify heroku setting

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.use(async function (err, req, res, next) {
     next();
 });
 
+if (process.env.NODE_ENV || "production" === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
+
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
