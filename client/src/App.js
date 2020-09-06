41b342b9a78e9e7816ca01de867c8401ef093723
Jidaewook <psatdoctor@naver.com>
2020-09-06 11:38:00 +0900

    #Add Client page start

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,14 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import Footer from './components/layout/Footer';
-import Nav from './components/layout/Navibar';
-import Landing from './components/layout/Landing';
-import Login from './auth/Login';
-import Dashboard from './components/Dashboard';
-import Store from './store';
-import bbs from './components/screen/bbs';
-import notice from './components/screen/notice';
-import psat from './components/screen/psat';
-import ncs from './components/screen/ncs';
-import user from './components/screen/user';
-
+import React from 'react';
 import './App.css';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser, logoutUser} from './components/actions/authActions';
 
-// Check for token
-if (localStorage.jwtToken) {
-  // Set auth token header auth
-  setAuthToken(localStorage.jwtToken);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(localStorage.jwtToken);
-  // Set user and isAuthenticated
-  Store.dispatch(setCurrentUser(decoded));
-
-  // check for expired token
-  const currentTime = Date.now() / 1000;
-  if (decoded.exp < currentTime) {
-      // Logged out user
-      Store.dispatch(logoutUser());
-
-      // Clear current Propfile
-      // store.dispatch(clearCurrentProfile());
-
-      // Redirect to login
-      window.location.href = '/login'
-  }
-
-};
-
-
-export default class App extends Component {
-  render(){
-    return (
-      <Provider store={Store}>
-        <Router>
-          <div className="App">
-            <Nav />
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path="/ncs" component={ncs}/>
-              <Route exact path="/psat" component={psat}/>
-              <Route exact path="/bbs" component={bbs}/>
-              <Route exact path="/notice" component={notice}/>
-              <Route exact path="/user" component={user}/>
-            </div>
-            <Footer />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hello world
+      </h1>
+    </div>
+  );
 }
 
+export default App;
